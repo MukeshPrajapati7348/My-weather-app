3e97b564adc02ca559d0acaff3aa8c9948816fda
Mukesh Prajapati <mukeshprajapat7348@gmail.com>
2023-03-20 14:13:25 +0530

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WeatherCompComponent } from './weather-comp/weather-comp.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'', component:WeatherCompComponent},
+{path:'**', redirectTo:''}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
